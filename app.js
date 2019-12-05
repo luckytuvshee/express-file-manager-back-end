f5a8e0e088c9c0c7573e251acc71fb3d4aa45c16
Tuvshinsanaa <luckytuvshee99@gmail.com>
2019-12-05 22:06:57 +0800

    Download file working with Chrome Ext

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ app.use(cors({ credentials: true, origin: true }));
 
 var contents = [];
 
+function rimraf(dir_path) {
+  if (fs.existsSync(dir_path)) {
+    fs.readdirSync(dir_path).forEach(function(entry) {
+      var entry_path = path.join(dir_path, entry);
+      if (fs.lstatSync(entry_path).isDirectory()) {
+        rimraf(entry_path);
+      } else {
+        fs.unlinkSync(entry_path);
+      }
+    });
+    fs.rmdirSync(dir_path);
+  }
+}
+
 function isDir(path) {
   try {
     var stat = fs.lstatSync(path);
@@ -18,14 +32,24 @@ function isDir(path) {
   }
 }
 
-fs.readdirSync(__dirname).forEach(file => {
-  contents.push({
-    file,
-    dir: __dirname,
-    path: __dirname + "/" + file,
-    is_dir: isDir(__dirname + "/" + file)
+function deleteFile(path, isFolder) {
+  if (isFolder) {
+    rimraf(path);
+  } else {
+    fs.unlinkSync(path);
+  }
+}
+
+function getContent(dirName) {
+  fs.readdirSync(__dirname).forEach(file => {
+    contents.push({
+      file,
+      dir: __dirname,
+      path: __dirname + "/" + file,
+      is_dir: isDir(__dirname + "/" + file)
+    });
   });
-});
+}
 
 function changeName(newPath, oldPath) {
   fs.rename(oldPath, newPath, function(err) {
@@ -38,16 +62,31 @@ function addDir(currentDir, newDirName) {
 }
 
 app.get("/", (req, res) => {
+  getContent(__dirname);
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ contents, currentDir: __dirname }));
+});
+
+app.get("/:folder", (req, res) => {
+  getContent(req.params.folder);
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ contents, currentDir: __dirname }));
 });
 
 app.post("/changeName", (req, res) => {
+  getContent(__dirname);
   changeName(req.body.newPath, req.body.oldPath);
   res.send(JSON.stringify(contents));
 });
 
+app.post("/delete", (req, res) => {
+  deleteFile(req.body.file, req.body.isFolder);
+  getContent(__dirname);
+  res.send(JSON.stringify(contents));
+});
+
 app.post("/addDir", (req, res) => {
+  getContent(__dirname);
   addDir(req.body.currentDir, req.body.dirName);
   res.send(JSON.stringify(contents));
 });
